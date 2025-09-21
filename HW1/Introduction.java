bc9255b678b1339cd36cac2cee879eb3c57d3032
phamgiahien123 <hienoof271006@gmail.com>
2025-09-21 10:17:19 +0700

    BaitapB

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args){
+        System.out.println("Pham Gia Hien\t24021465\tCS5\trahien123\thienoof271006@gmail.com");
+        for(int i=9;i>=1;i--){
+            System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
